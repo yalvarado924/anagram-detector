b2b63af3d3513686a171092d35949026d1ead94c
Yuliana Alvarado <yalvarado924@gmail.com>
2020-11-17 14:46:48 -0600

    commit tree 8f383e97401b74df3987b281209a4dba7216ab8c

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(words)
+        new_array = []
+        words.each do |word|
+            new_array << word if word.split("").sort == @word.split("").sort
+        end
+        new_array
+    end
+end
